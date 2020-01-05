f0a38d690cbb4b1342bb5c2ea426b3ea94f6690d
Vadim Bakaev <bakaev.vad@gmail.com>
2020-01-05 19:28:50 +0100

    Refactoring to facilitate tests

--- src/main/scala/vbakaev/app/Main.scala
+++ src/main/scala/vbakaev/app/Main.scala
@@ -8,14 +8,14 @@ import akka.stream.scaladsl._
 import com.typesafe.scalalogging.LazyLogging
 import pureconfig._
 import pureconfig.generic.auto._
-import vbakaev.app.chat.{ChatConnectionHandler, ChatConnectionHandlerImpl}
+import vbakaev.app.chat.{ChatConnectionHandlerProvider, ChatConnectionHandlerProviderImpl}
 import vbakaev.app.config.AppConfig
 
 object Main extends App with LazyLogging {
   logger.info("Application started")
 
-  implicit val actorSystem: ActorSystem            = ActorSystem("simple-chat")
-  val chatConnectionHandler: ChatConnectionHandler = new ChatConnectionHandlerImpl()
+  implicit val actorSystem: ActorSystem       = ActorSystem("simple-chat")
+  val provider: ChatConnectionHandlerProvider = new ChatConnectionHandlerProviderImpl()
 
   ConfigSource.default
     .load[AppConfig]
@@ -27,8 +27,11 @@ object Main extends App with LazyLogging {
       config => {
         RunnableGraph
           .fromGraph(GraphDSL.create() { implicit builder =>
-            val tcpConnection        = Tcp().bind(config.tcp.interface, config.tcp.port)
-            val tcpConnectionHandler = Sink.foreach[Tcp.IncomingConnection](chatConnectionHandler.handleConnection)
+            val tcpConnection = Tcp().bind(config.tcp.interface, config.tcp.port)
+            val tcpConnectionHandler = Sink.foreach[Tcp.IncomingConnection] { connection =>
+              connection.handleWith(provider.connectionHandler())
+              ()
+            }
 
             tcpConnection ~> tcpConnectionHandler
 
---
+++ src/main/scala/vbakaev/app/chat/ChatConnectionHandlerProvider.scala
@@ -0,0 +1,37 @@
+package vbakaev.app.chat
+
+import java.util.UUID
+
+import akka.NotUsed
+import akka.stream.Materializer
+import akka.stream.scaladsl.{BroadcastHub, Flow, Keep, MergeHub}
+import akka.util.ByteString
+import vbakaev.app.chat.ChatConnectionHandlerProviderImpl.MessageWrapper
+
+trait ChatConnectionHandlerProvider {
+
+  def connectionHandler(): Flow[ByteString, ByteString, NotUsed]
+
+}
+
+class ChatConnectionHandlerProviderImpl(
+    generateRandomUUID: () => UUID = UUID.randomUUID
+)(implicit mat: Materializer)
+    extends ChatConnectionHandlerProvider {
+  private val (sink, source) = MergeHub.source[MessageWrapper].toMat(BroadcastHub.sink[MessageWrapper])(Keep.both).run()
+
+  override def connectionHandler(): Flow[ByteString, ByteString, NotUsed] = {
+    val userId = generateRandomUUID()
+
+    Flow
+      .fromFunction(MessageWrapper(userId, _))
+      .via(Flow.fromSinkAndSource(sink, source))
+      .collect {
+        case MessageWrapper(senderId, msg) if senderId != userId => msg
+      }
+  }
+}
+
+object ChatConnectionHandlerProviderImpl {
+  private final case class MessageWrapper(uuid: UUID, msg: ByteString)
+}
